0f34c37bef6c19e3ad2b934245cbdf8d3959ffce
Murat Yakin <yakin_murat@yahoo.com>
2019-11-15 15:04:16 +0300

    Ders 2 sonu

---
+++ Assets/0-Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        float hor = Input.GetAxis("Horizontal");
+        if (hor<0) {
+            transform.rotation = Quaternion.Euler(0,180,0);
+        } else {
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        animator.SetFloat("move", hor);
+        
+
+        // muratyakin@muratyakin.com
+
+        // if (Input.GetKey(KeyCode.D)) {
+        //     animator.SetBool("isWalking", true);
+        // }
+        // if (Input.GetKey(KeyCode.A)) {
+        //     animator.SetBool("isWalkingLeft", true);
+        //     transform.rotation = Quaternion.Euler(0,180,0);
+        // }
+        // if (Input.GetKeyUp(KeyCode.D)) {
+        //     animator.SetBool("isWalking", false);
+        // }
+        // if (Input.GetKeyUp(KeyCode.A)) {
+        //     animator.SetBool("isWalkingLeft", false);
+        //      transform.rotation = Quaternion.Euler(0,0,0);
+        // }
+    }
+
+}
---
+++ Assets/0-Scripts/PlayerSpriteAnimations.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteAnimations : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] idleSprites;
+    public Sprite[] walkingSprites;
+    public float animationSpeed = 0.05f;
+    private int indexNo;
+
+    private bool isWalking;
+
+
+    private void Start() {
+        StartCoroutine(AnimateIdle());
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.D)) {
+            if (!isWalking) {
+                isWalking=true;
+                indexNo=0;
+                StartCoroutine(AnimateWalk());
+            }
+                
+        }
+    }
+
+    public IEnumerator AnimateIdle() {
+        spriteRenderer.sprite = idleSprites[indexNo];
+        indexNo++;
+        if (indexNo==14) 
+            indexNo = 0;
+        yield return new WaitForSeconds(animationSpeed);
+        if (isWalking) {
+            StopCoroutine(AnimateIdle());
+        } else {
+            yield return AnimateIdle();
+        }
+        
+    }
+
+    public IEnumerator AnimateWalk() {
+        spriteRenderer.sprite = walkingSprites[indexNo];
+        indexNo++;
+        if (indexNo==15) 
+            indexNo = 0;
+        yield return new WaitForSeconds(animationSpeed);
+        if(!isWalking) {
+            StopCoroutine(AnimateWalk());
+        } else {
+            yield return AnimateWalk();
+        }
+        
+    }
+
+}
