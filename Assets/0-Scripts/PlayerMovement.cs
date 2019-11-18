1daabb0f03c5d03d75b30bacedd000ce447ebe2f
Murat Yakin <yakin_murat@yahoo.com>
2019-11-18 11:22:11 +0300

    Ders 03 ogle arasina kadar

--- Assets/0-Scripts/PlayerMovement.cs
+++ Assets/0-Scripts/PlayerMovement.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float walkRate;
+    public float runRate;
+    public float jumpStrength;
     private Animator animator;
+    private float multiplier = 1f;
+    private bool isJumping;
+    private float translationMultiplier=1;
 
     private void Start() {
         animator = GetComponent<Animator>();
     }
 
     private void Update() {
-        float hor = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            multiplier = 2f;
+        } else {
+            multiplier = 1f;
+        }
+        
+        float hor = Input.GetAxis("Horizontal") * multiplier;
+        
         if (hor<0) {
             transform.rotation = Quaternion.Euler(0,180,0);
+            translationMultiplier = -1f;
         } else {
             transform.rotation = Quaternion.Euler(0,0,0);
+            translationMultiplier = 1f;
         }
         animator.SetFloat("move", hor);
+        if (multiplier==1) {
+            transform.position += transform.right * (hor * walkRate * translationMultiplier);
+        } else {
+            transform.position += transform.right * (hor * runRate * translationMultiplier);
+        }
+        //transform.position = new Vector3(transform.position.x+walkRate, transform.position.y, transform.position.z);
+        
+        if (!animator.GetAnimatorTransitionInfo(0).anyState && !animator.GetCurrentAnimatorStateInfo(0).IsName("Ellen_Jump_Animation")) {
+            isJumping = false;
+        }
+
+        if (!isJumping && Input.GetKeyDown(KeyCode.Space)) {
+            isJumping = true;
+            GetComponent<Rigidbody>().AddForce(transform.up * jumpStrength);
+            animator.SetTrigger("jump");
+        }
+        
         
 
         // muratyakin@muratyakin.com
